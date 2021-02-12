29a90932f8f456b3eecdafbc2e65377f89dee21a
dibdas <dibyandu08das@gmail.com>
2021-02-12 12:50:27 +0530

    added stylelint
    made sure the events are private

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
-  resources :posts
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :posts, only: [:new, :create, :index]
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
